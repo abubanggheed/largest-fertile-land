37459958c19e6e8d07f1ccffe4575355ef2be24e
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2019-03-23 18:03:13 -0500

    built function to run algorithm multiple times as posed in the problem

--- largest_fertile_land.py
+++ largest_fertile_land.py
@@ -1,4 +1,19 @@
 from calculate_land import Land_Plot
 
 def largest_fertile_land(input_string):
-    return input_string
+    input_list = input_string.splitlines()
+    plot_matrix = []
+    i = 1
+    m_end = input_list[0][0] + 1
+    while i < m_end:
+        plot_matrix.append(input_list[i])
+        i += 1
+    out_string = ''
+    i += 1
+    while i < len(input_list):
+        top_row = input_list[i][0]
+        bottom_row = input_list[i][-1]
+        plot = Land_Plot(plot_matrix[top_row:bottom_row], 'F')
+        out_string += str(plot.process_all_rows()) + '\n'
+        i += 1
+    return out_string
