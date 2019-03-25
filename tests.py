e605d285e1f784993cbff2010581626fe09b7ced
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2019-03-25 13:06:55 -0500

    added 2 more tests

--- tests.py
+++ tests.py
@@ -1,16 +1,29 @@
 from largest_fertile_land import largest_fertile
 
 inputs = [
-    '5 6\nFBBFFB\nFBFFBB\nFFBBFF\nBBFBFF\nBFBBFF\n4\n1 3\n2 4\n1 5\n3 5'
+    '5 6\nFBBFFB\nFBFFBB\nFFBBFF\nBBFBFF\nBFBBFF\n4\n1 3\n2 4\n1 5\n3 5',
+    '3 6\nBBFFFF\nFFFFBB\nBBFFBB\n2\n1 3\n1 2',
+    '4 8\nFFBBFFFF\nFFFFFFBB\nFFBBFFBB\nFFBBBBBB\n3\n1 4\n1 3\n2 4'
 ]
 
 expected_outputs = [
-    '3\n4\n6\n6\n'
+    '3\n4\n6\n6\n',
+    '6\n4\n',
+    '8\n6\n6\n'
 ]
 
 outputs = []
 for i in range(0, len(inputs)):
     outputs.append(largest_fertile(inputs[i]))
 
+passed = 0
 for i in range(0, len(outputs)):
-    print(outputs[i] == expected_outputs[i])
+    if(outputs[i] == expected_outputs[i]):
+        passed += 1
+        print('test ' + str(i + 1) + ' of ' + str(len(outputs)) + ' passed')
+    else:
+        print('test ' + str(i + 1) + ' of ' + str(len(outputs)) + ' failed')
+if(passed == len(outputs)):
+    print('all tests passed')
+else:
+    print('not all tests passed')
