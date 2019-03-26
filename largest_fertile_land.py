0c7d8d7316385d95d79662e03755718efad9c6cd
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2019-03-26 15:57:42 -0500

    adjusted main process to only find the rects for a given plot once

--- largest_fertile_land.py
+++ largest_fertile_land.py
@@ -1,4 +1,4 @@
-from calculate_land import Land_Plot
+from calculate_land import Land_Plot, Rect
 
 def largest_fertile(input_string):
     input_list = input_string.splitlines()
@@ -10,10 +10,26 @@ def largest_fertile(input_string):
         i += 1
     out_string = ''
     i += 1
+    plot = Land_Plot(plot_matrix, 'F')
+    plot.process_all_rows()
+    rects_to_consider = plot.all_rects
     while i < len(input_list):
         top_row = int(input_list[i][0]) - 1
         bottom_row = int(input_list[i][-1])
-        plot = Land_Plot(plot_matrix[top_row:bottom_row], 'F')
-        out_string += str(plot.process_all_rows().area()) + '\n'
+        boundary_rect = Rect(0, len(plot_matrix[0]), top_row, bottom_row - 1)
+        largest_area = find_largest_area(boundary_rect, rects_to_consider, 0)
+        out_string += str(largest_area) + '\n'
         i += 1
     return out_string
+
+def find_largest_area(boundary, rects_list, cropped_area, i = 0):
+    if i >= len(rects_list):
+        return cropped_area
+    elif cropped_area >= rects_list[i].area():
+        return cropped_area
+    else:
+        return find_largest_area(boundary, rects_list, update_cropped(boundary, rects_list[i], cropped_area), i + 1)
+
+def update_cropped(boundary, rect, current_area):
+    inner_rect = Rect(rect.lindex, rect.rindex, max([rect.start, boundary.start]), min([rect.end, boundary.end]))
+    return max([inner_rect.area(), current_area])
