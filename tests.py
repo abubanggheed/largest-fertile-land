ca122a06f62e75dd69defa276644319122dfd811
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2019-03-26 15:39:53 -0500

    added 4th test

--- tests.py
+++ tests.py
@@ -3,13 +3,15 @@ from largest_fertile_land import largest_fertile
 inputs = [
     '5 6\nFBBFFB\nFBFFBB\nFFBBFF\nBBFBFF\nBFBBFF\n4\n1 3\n2 4\n1 5\n3 5',
     '3 6\nBBFFFF\nFFFFBB\nBBFFBB\n2\n1 3\n2 3',
-    '4 8\nFFBBFFFF\nFFFFFFBB\nFFBBFFBB\nFFBBBBBB\n3\n1 4\n1 3\n2 4'
+    '4 8\nFFBBFFFF\nFFFFFFBB\nFFBBFFBB\nFFBBBBBB\n3\n1 4\n1 3\n2 4',
+    '2 2\nFB\nBB\n1\n2 2'
 ]
 
 expected_outputs = [
     '3\n4\n6\n6\n',
     '6\n4\n',
-    '8\n6\n6\n'
+    '8\n6\n6\n',
+    '0\n'
 ]
 
 outputs = []
